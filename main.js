ab6700cce7b1f4c1cb30ae10365486d4ba99a9ce
Mathew <hunter3142@gmail.com>
2017-09-13 01:30:39 -0400

    Move paddle with arrow keys

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
+var animate = window.requestAnimationFrame || 
+  function(callback) { window.setTimeout(callback, 1000/60) };
+
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
-context.fillRect(25, 25, 700, 550);
+
 window.onload = function() {
-	renderBoard();
+  animate(step);
+}
+
+function step() {
+  renderBoard();
+  animate(step);
 }
 
 function Paddle(x_position, y_position) {
 	this.x_position = x_position;
 	this.y_position = y_position;
-	this.width = 25;
+	this.width = 15;
 	this.height = 100;
 }
 
@@ -20,10 +28,15 @@ Paddle.prototype.render = function() {
 	context.fill();
 }
 
+Paddle.prototype.move = function(y_change) {
+  context.clearRect(this.x_position, this.y_position, this.width, this.height)
+  this.y_position += y_change;
+}
+
 function Ball() {
 	this.x = canvas.width / 2;
 	this.y = canvas.height / 2;
-	this.radius = 25;
+	this.radius =10;
 	this.start_angle = 0;
 	this.end_angle = 2 * Math.PI;
 
@@ -46,6 +59,23 @@ function renderBoard() {
 	computer.render();
 }
 
+function keyDown(key) {
+  if (key.keyCode == 38) { //up key
+    if (playerOne.y_position >= 25) {
+      playerOne.move(-10)
+    }
+  }
+  else if (key.keyCode == 40) { //down key
+    if (playerOne.y_position <= 475) {
+      playerOne.move(10)
+    }
+  }
+}
+
 var playerOne = new Paddle(50, 50);
 var computer = new Paddle(675, 50);
 var gameBall = new Ball();
+
+
+window.addEventListener("keydown", keyDown, true);
+
